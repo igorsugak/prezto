ce76492005153628556585fdf7b6b40d4f3110ba
Igor Sugak <sugak@fb.com>
2016-05-29 16:41:25 -0700

    More tweaking

--- modules/history/init.zsh
+++ modules/history/init.zsh
@@ -11,28 +11,27 @@
 # Variables
 #
 
-HISTFILE="${ZDOTDIR:-$HOME}/.zsh-history"       # The path to the history file.
-HISTSIZE=130000                   # The maximum number of events to save in the internal history.
-SAVEHIST=130000                   # The maximum number of events to save in the history file.
+HISTFILE="${ZDOTDIR:-$HOME}/.zsh-history" # The path to the history file.
+HISTSIZE=130000                # The maximum number of events to save.
+SAVEHIST=130000                # The maximum number of events to save.
 
 #
 # Options
 #
 
-setopt BANG_HIST                 # Treat the '!' character specially during expansion.
-setopt EXTENDED_HISTORY          # Write the history file in the ':start:elapsed;command' format.
-setopt INC_APPEND_HISTORY        # Write to the history file immediately, not when the shell exits.
-setopt SHARE_HISTORY             # Share history between all sessions.
-setopt HIST_EXPIRE_DUPS_FIRST    # Expire a duplicate event first when trimming history.
-setopt HIST_IGNORE_DUPS          # Do not record an event that was just recorded again.
-setopt HIST_IGNORE_ALL_DUPS      # Delete an old recorded event if a new event is a duplicate.
-setopt HIST_FIND_NO_DUPS         # Do not display a previously found event.
-setopt HIST_IGNORE_SPACE         # Do not record an event starting with a space.
-setopt HIST_SAVE_NO_DUPS         # Do not write a duplicate event to the history file.
-setopt HIST_VERIFY               # Do not execute immediately upon history expansion.
-setopt HIST_BEEP                 # Beep when accessing non-existent history.
-setopt HIST_NO_FUNCTIONS         # Do not store function definitions in history.
-setopt HIST_REDUCE_BLANKS        # Remove superfluous blanks from each command being added to the history.
+setopt BANG_HIST               # Treat the '!' character specially.
+setopt EXTENDED_HISTORY        # Use ':start:elapsed;command' history format.
+setopt INC_APPEND_HISTORY      # Write to the history file immediately.
+setopt SHARE_HISTORY           # Share history between all sessions.
+setopt HIST_EXPIRE_DUPS_FIRST  # Expire a duplicate event first when trimming.
+setopt HIST_IGNORE_DUPS        # Do not record an event that was just recorded.
+setopt HIST_IGNORE_ALL_DUPS    # Delete an old event if a new one is a duplicate.
+setopt HIST_FIND_NO_DUPS       # Do not display a previously found event.
+setopt HIST_IGNORE_SPACE       # Do not record an event starting with a space.
+setopt HIST_SAVE_NO_DUPS       # Do not write a duplicate event.
+setopt HIST_VERIFY             # Do not execute immediately upon expansion.
+setopt HIST_NO_FUNCTIONS       # Do not store function definitions in history.
+setopt HIST_REDUCE_BLANKS      # Remove superfluous blanks from each command.
 
 #
 # Aliases
